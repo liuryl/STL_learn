deefcc3bdfb5c58d3e5114afd59448dc3f14ad7a
liuryl <Liuryl@outlook.com>
2023-03-23 09:22:27 +0800

    Initial commit

---
+++ leecode1.cpp
@@ -0,0 +1,8 @@
+//leecode1，使用了哈希，用map建立哈希表，vector建立一个容器承载返回值
+#include <iostream>
+#include <map>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::map<int,int> num;
+    return 0;
+}
